b6c0935f9b07485668113950598ce2c0a34cb76b
Mina <by.mouna@gmail.com>
2022-01-25 16:46:18 +0100

    firstcommit

--- src/components/Person.js
+++ src/components/Person.js
@@ -7,9 +7,28 @@ export default class Person extends Component {
             fullName:"By Mina",
             bio:"This is my bio",
             imgSrc:"https://images.unsplash.com/photo-1639628735078-ed2f038a193e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-            profession:"Web Developer"
+            profession:"Web Developer",
+            timer:0
+
         }
+        
     }
+    
+
+
+    componentDidMount() {
+        console.log("component DidMount")
+        setInterval(()=>this.setState({...this.state,timer:this.state.timer+1}),1000)
+    }
+
+    componentDidUpdate() {
+        console.log("component updated")
+    }
+
+    componentWillUnmount() {
+        console.log("component WillUnMount")
+    }
+    
 
     render() {
         return (
@@ -20,6 +39,7 @@ export default class Person extends Component {
                     BIO: {this.state.bio}<br/>
                     PROFESSION: {this.state.profession}
                 </div>
+                <h1>{this.state.timer}</h1>
                 
             </div>
         )
